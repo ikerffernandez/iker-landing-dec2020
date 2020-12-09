137e70d999539b8035c86939be30b61b056ea54b
Iker <41619735+ikerffernandez@users.noreply.github.com>
2020-12-09 18:02:55 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,25 +1,39 @@
-$(".logo-image, .title, .tagline").mouseenter(
+$(".toggle").click(
   function () {
-    $('img').addClass('img-visible');
-    $('.image-1').addClass('image-1-visible');  
-    $('.image-2').addClass('image-2-visible'); 
-    $('.image-3').addClass('image-3-visible');     
-    $('.image-4').addClass('image-4-visible');
-    $('.image-5').addClass('image-5-visible');     
-    $('.image-6').addClass('image-6-visible');  
-    $('.logo-image').addClass('logo-image-hover');                 
+    $('img').toggleClass('img-visible');
+    $('.image-1').toggleClass('image-1-visible');  
+    $('.image-2').toggleClass('image-2-visible'); 
+    $('.image-3').toggleClass('image-3-visible');     
+    $('.image-4').toggleClass('image-4-visible');
+    $('.image-5').toggleClass('image-5-visible');     
+    $('.image-6').toggleClass('image-6-visible');   
+    $('.toggle').toggleClass('toggle-active');                
       }, 
 );
 
-$(".logo-image, .title, .tagline").mouseleave(
-  function () {
-    $('img').removeClass('img-visible');
-    $('.image-1').removeClass('image-1-visible');    
-    $('.image-2').removeClass('image-2-visible');  
-    $('.image-3').removeClass('image-3-visible');         
-    $('.image-4').removeClass('image-4-visible');  
-    $('.image-5').removeClass('image-5-visible');    
-    $('.image-6').removeClass('image-6-visible');   
-    $('.logo-image').removeClass('logo-image-hover');                                      
-  }, 
+
+$(".toggle").mouseover(
+  function () { 
+    $('.toggle').addClass('toggle-hover');                
+      }, 
 );
+
+$(".toggle").mouseout(
+  function () { 
+    $('.toggle').removeClass('toggle-hover');                
+      }, 
+);
+
+
+// $(".logo-image, .title, .tagline").click(
+//   function () {
+//     $('img').removeClass('img-visible');
+//     $('.image-1').removeClass('image-1-visible');    
+//     $('.image-2').removeClass('image-2-visible');  
+//     $('.image-3').removeClass('image-3-visible');         
+//     $('.image-4').removeClass('image-4-visible');  
+//     $('.image-5').removeClass('image-5-visible');    
+//     $('.image-6').removeClass('image-6-visible');   
+//     $('.logo-image').removeClass('logo-image-hover');                                      
+//   }, 
+// );
