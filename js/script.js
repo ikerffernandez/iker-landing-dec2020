277fc1f40d292611d7974da1f0902937643991aa
Iker <41619735+ikerffernandez@users.noreply.github.com>
2020-12-09 16:15:15 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ $(".logo-image, .title, .tagline").mouseenter(
     $('.image-3').addClass('image-3-visible');     
     $('.image-4').addClass('image-4-visible');
     $('.image-5').addClass('image-5-visible');     
-    $('.image-6').addClass('image-6-visible');                  
+    $('.image-6').addClass('image-6-visible');  
+    $('.logo-image').addClass('logo-image-hover');                 
       }, 
 );
 
@@ -18,6 +19,7 @@ $(".logo-image, .title, .tagline").mouseleave(
     $('.image-3').removeClass('image-3-visible');         
     $('.image-4').removeClass('image-4-visible');  
     $('.image-5').removeClass('image-5-visible');    
-    $('.image-6').removeClass('image-6-visible');                    
+    $('.image-6').removeClass('image-6-visible');   
+    $('.logo-image').removeClass('logo-image-hover');                                      
   }, 
 );
