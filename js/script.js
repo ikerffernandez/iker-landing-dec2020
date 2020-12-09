8a942f5c0d592636bbe7e7897066852ebe27bf12
Iker <41619735+ikerffernandez@users.noreply.github.com>
2020-12-09 15:39:02 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(".logo-image, .title, .tagline").mouseenter(
+  function () {
+    $('img').addClass('img-visible');
+    $('.image-1').addClass('image-1-visible');  
+    $('.image-2').addClass('image-2-visible'); 
+    $('.image-3').addClass('image-3-visible');     
+    $('.image-4').addClass('image-4-visible');
+    $('.image-5').addClass('image-5-visible');     
+    $('.image-6').addClass('image-6-visible');                  
+      }, 
+);
+
+$(".logo-image, .title, .tagline").mouseleave(
+  function () {
+    $('img').removeClass('img-visible');
+    $('.image-1').removeClass('image-1-visible');    
+    $('.image-2').removeClass('image-2-visible');  
+    $('.image-3').removeClass('image-3-visible');         
+    $('.image-4').removeClass('image-4-visible');  
+    $('.image-5').removeClass('image-5-visible');    
+    $('.image-6').removeClass('image-6-visible');                    
+  }, 
+);
